d8c8a636344ea6d41ba5db15b3a8209c92d452dd
nyogimi <rhenzfernandez.rf@gmail.com>
2023-12-02 07:24:47 +0800

    update2

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import streamlit as st
 import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
 
-@st.cache_resource
+# Load the model using st.cache
+@st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('tomato.h5')
-  return model
-model=load_model()
+    model = tf.keras.models.load_model('tomato.h5')
+    return model
+
+model = load_model()
+
 st.write("""
-# Tomato Leaf Disease Classification Using
- Image Recognition """
-)
+# Tomato Leaf Disease Classification Using Image Recognition
+""")
+
 st.text(" CPE 027-CPE41S4 - Digital Signal Processing and Application")
 st.text(" Members:")
 st.text(" Aragon, Roujienald")
@@ -18,38 +23,37 @@ st.text(" Geslani, Grant Guriel")
 st.text(" Instructor: Engr. Bonry Dorado")
 file=st.file_uploader("Choose tomato leaf photo from your computer",type=["jpg"])
 
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
-def import_and_predict(image_data,model):
-    size=(224,224)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
-    return prediction
+# Add file uploader
+file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
+# Check if a file is uploaded
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['Pepper__bell___Bacterial_spot',
-                'Pepper__bell___healthy',
-                'Potato___Early_blight',
-                'Potato___Late_blight',
-                'Potato___healthy',
-                'Tomato_Bacterial_spot',
-                'Tomato_Early_blight',
-                'Tomato_Late_blight',
-                'Tomato_Leaf_Mold',
-                'Tomato_Septoria_leaf_spot',
-                'Tomato_Spider_mites_Two_spotted_spider_mite',
-                'Tomato__Target_Spot',
-                'Tomato__Tomato_YellowLeaf__Curl_Virus',
-                'Tomato__Tomato_mosaic_virus',
-                'Tomato_healthy']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
-    st.success(string)
-    
+    # Process the uploaded image
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+
+    # Resize and preprocess the image for prediction
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis, ...]
+
+    # Make prediction using the loaded model
+    prediction = model.predict(img_reshape)
+
+    # Define class names
+    class_names = [
+        'Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy',
+        'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy',
+        'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight',
+        'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot',
+        'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot',
+        'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus',
+        'Tomato_healthy'
+    ]
+
+    # Display the predicted class
+    predicted_class = class_names[np.argmax(prediction)]
+    st.success(f"OUTPUT: {predicted_class}")
